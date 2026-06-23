ca41a5677f8ed5466d60fa0cbfe2e61c63e7beae
Mohamadou Mor Sangare <mohamadoumor.sangare@unck.edu.sn>
2026-06-23 20:31:02 +0000

    refactor: fix relationships in Classe and AffectationController

--- backend/app/Http/Controllers/Api/AffectationController.php
+++ backend/app/Http/Controllers/Api/AffectationController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Classe;
-use App\Models\Matiere;
+use App\Models\Matieres;
 use App\Models\User;
 use Illuminate\Http\Request;
 
