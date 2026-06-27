2377d4de38b358faca89550e3b90ca7913000372
Mohamadou Mor Sangare <mohamadoumor.sangare@unck.edu.sn>
2026-06-27 17:07:35 +0000

    feat: add matieres relationship to Classe model

--- backend/app/Models/classe.php
+++ backend/app/Models/classe.php
@@ -25,8 +25,7 @@ class Classe extends Model
     {
         return $this->hasMany(EmploiDuTemps::class, 'id_classe');
     }
-
-    public function matieres()
+     public function matieres()
     {
         return $this->belongsToMany(Matieres::class, 'classe_matiere', 'id_classe', 'id_matiere');
     }
