bc2c990c61627d66b63f9ceaf719c53211adb17e
Mohamadou Mor Sangare <mohamadoumor.sangare@unck.edu.sn>
2026-07-01 17:01:16 +0000

    modidier

--- backend/app/Models/classe.php
+++ backend/app/Models/classe.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Classe extends Model
 {
@@ -24,5 +25,15 @@ class Classe extends Model
     public function emploiDuTemps()
     {
         return $this->hasMany(EmploiDuTemps::class, 'id_classe');
+    }
+
+    /**
+     * Définit la relation plusieurs-à-plusieurs avec le modèle Matiere.
+     * Une classe peut avoir plusieurs matières.
+     */
+    public function matieres(): BelongsToMany
+    {
+        // Le contrôleur d'affectation a besoin de cette relation pour fonctionner.
+        return $this->belongsToMany(Matieres::class, 'classe_matiere', 'id_classe', 'id_matiere');
     }
 }
