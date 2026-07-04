883a08d91dd01af976c550c0e1a68e76bbd982a3
ndiasse-fall <ndiasse.fall@unchk.edu.sn>
2026-07-04 21:58:17 +0000

    Render déploie

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
+Route::get('/run-migrations', function () {
+    try {
+        // Le paramètre --force est obligatoire en production
+        Artisan::call('migrate', ['--force' => true]);
+        return 'Les migrations ont été exécutées avec succès 🎉 !';
+    } catch (\Exception $e) {
+        return 'Erreur lors de la migration : ' . $e->getMessage();
+    }
+});
+Route::get('/run-seeders', function () {
+    try {
+        // Le paramètre --force est obligatoire pour exécuter des seeders en production
+        Artisan::call('db:seed', ['--force' => true]);
+        return 'Les données de test ont été insérées avec succès 🎉 !';
+    } catch (\Exception $e) {
+        return 'Erreur lors du seeding : ' . $e->getMessage();
+    }
+});
 Route::get('/', function () {
     return view('welcome');
 });
